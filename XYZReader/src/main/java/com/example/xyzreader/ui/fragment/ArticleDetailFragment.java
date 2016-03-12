b04dc3dacd0de2aec71b824f185a555676ded18e
nvalletta <nora.valletta@gmail.com>
2016-03-12 09:05:10 -0700

    Package refactor

--- XYZReader/src/main/java/com/example/xyzreader/ui/fragment/ArticleDetailFragment.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/fragment/ArticleDetailFragment.java
@@ -1,4 +1,4 @@
-package com.example.xyzreader.ui;
+package com.example.xyzreader.ui.fragment;
 
 import android.app.Fragment;
 import android.app.LoaderManager;
@@ -27,6 +27,11 @@ import com.android.volley.VolleyError;
 import com.android.volley.toolbox.ImageLoader;
 import com.example.xyzreader.R;
 import com.example.xyzreader.data.ArticleLoader;
+import com.example.xyzreader.ui.DrawInsetsFrameLayout;
+import com.example.xyzreader.ui.ImageLoaderHelper;
+import com.example.xyzreader.ui.ObservableScrollView;
+import com.example.xyzreader.ui.activity.ArticleDetailActivity;
+import com.example.xyzreader.ui.activity.ArticleListActivity;
 
 /**
  * A fragment representing a single Article detail screen. This fragment is
